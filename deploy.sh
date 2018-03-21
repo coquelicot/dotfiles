6124ade7b44ccbdd40e80b38d0303f36690e9778
fcrh <coquelicot1117@gmail.com>
2018-03-21 15:45:01 +0800

    Add diff-so-fancy & fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,25 @@
 DIR=$(cd "$(dirname -- "$0")" && pwd)
 SRC_DIR="$DIR/src"
 
-for file in `find "$SRC_DIR" -maxdepth 1 -type f`; do
-    ln -sf "$SRC_DIR/$file" "$HOME/.$file"
+# update submodule
+
+cd "$DIR"
+git submodule init
+git submodule update
+
+# install
+
+for fpath in `find "$SRC_DIR" -maxdepth 1 -type f`; do
+    fname=`basename "$fpath"`
+    ln -sf "$fpath" "$HOME/.$fname"
 done
+
+# set git pager
+
+GIT_CFG="$HOME/.gitconfig.local"
+GIT_PAGER="\"$SRC_DIR/diff-so-fancy/diff-so-fancy\" | less --tabs=4 -RFX"
+
+if [ "`git config -f "$GIT_CFG" --get core.pager`" != "$GIT_PAGER" ]; then
+    git config -f "$GIT_CFG" --unset-all core.pager
+    git config -f "$GIT_CFG" --add core.pager "$GIT_PAGER"
+fi
